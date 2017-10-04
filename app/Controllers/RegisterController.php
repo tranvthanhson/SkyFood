0d5f9d1b58a81ec10ea228f718f7f993cbb206f5
Hoai Thuong <thuong.hth@neo-lab.vn>
2017-10-04 11:01:32 +0700

    *[thuong] Done Login

--- app/Controllers/RegisterController.php
+++ app/Controllers/RegisterController.php
@@ -10,4 +10,29 @@ class RegisterController
     {
         return view('register/index');
     }
+
+    public function register()
+    {
+        if (isset($_POST['register'])) {
+            $user = [];
+            $user['USERNAME'] = $_POST['username'];
+            $user['PASSWORD'] = md5($_POST['password']);
+            $user['FIRST_NAME'] = $_POST['first_name'];
+            $user['LAST_NAME'] = $_POST['last_name'];
+            $user['EMAIL'] = $_POST['email'];
+            $user['FULL_NAME'] = $user['LAST_NAME'] . ' ' . $user['FIRST_NAME'];
+            $user['ROLE'] = 3;
+            $th = App::get('database')->testUserAlready('ACCOUNT', $user['USERNAME']);
+            if (null != $th[0]) {
+                echo 'Username already!';
+            } else {
+                $check = App::get('database')->insert('ACCOUNT', $user);
+                if (isset($check)) {
+                    echo 'cc';
+                } else {
+                    echo 'kk';
+                }
+            }
+        }
+    }
 }
