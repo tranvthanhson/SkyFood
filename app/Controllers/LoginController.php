ac08fac7478b6a453159a0411073ccef25bacd44
Hoai Thuong <thuong.hth@neo-lab.vn>
2017-10-04 09:52:42 +0700

    -[thuong] remove controller folder

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -10,4 +10,25 @@ class LoginController
     {
         return view('login/index');
     }
+
+    public function login()
+    {
+        if (isset($_POST['login'])) {
+            extract($_POST);
+            $username = $_POST['username'];
+            $password = md5($_POST['password']);
+
+            $th = App::get('database')->testUser('ACCOUNT', $username, $password);
+
+            if (($th[0]->USERNAME == $username) && ($th[0]->PASSWORD == $password)) {
+                if ((1 == $th[0]->ROLE) || (2 == $th[0]->ROLE)) {
+                    $_SESSION['user'] = $th[0];
+                    //var_dump($_SESSION['user']);
+                    return redirect('users');
+                }
+            } else {
+                return redirect('');
+            }
+        }
+    }
 }
