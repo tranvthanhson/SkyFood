3789519839f4418eae91e4d9ff18a779a29bb506
tranvthanhson <tranvthanhson@gmail.com>
2017-10-05 16:29:00 +0700

    Revert "@[hieu-frontend] Edit _global.scss"

--- index.php
+++ index.php
@@ -4,12 +4,10 @@
 // ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
 
-require "core/Autoload.php";
-new core\Autoload;
-// require "vendor/autoload.php";
+require "vendor/autoload.php";
 require "core/bootstrap.php";
 
-use core\{Router, Request};
+use App\Core\{Router, Request};
 
 Router::load("app/routes.php")
 	->direct(Request::uri(), Request::method());
