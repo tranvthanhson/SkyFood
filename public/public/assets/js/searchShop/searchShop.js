8085ff43b87b0f99852bc9078081656460ec0fe7
Son, Tran Vu Thanh <tranvthanhson@gmail.com>
2017-10-19 16:32:51 +0700

    * fix bug click search not search

--- public/public/assets/js/searchShop/searchShop.js
+++ public/public/assets/js/searchShop/searchShop.js
@@ -1,22 +1,22 @@
 $('#input').on('keydown',function (e) {
     if(e.which==13){
       $('#searchId').click();
-      $(".se-pre-con").fadeIn();;
   }
 });
 
 function search(link){
- var key = $('#input').val();
- var type = $('#type').val();
- var sortBy = $('#sortBy').val();
- $.ajax({
-    url: link,
-    type: 'GET',
-    cache: false,
-    data: {
-        ajaxKey: key,
-        type: type,
-        sortBy: sortBy
+    $(".se-pre-con").fadeIn();;
+    var key = $('#input').val();
+    var type = $('#type').val();
+    var sortBy = $('#sortBy').val();
+    $.ajax({
+        url: link,
+        type: 'GET',
+        cache: false,
+        data: {
+            ajaxKey: key,
+            type: type,
+            sortBy: sortBy
         // active: active
     },
     success: function(data) {
