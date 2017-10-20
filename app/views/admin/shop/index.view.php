dce22bfa417a484c5bc692347115ba3739d90e8f
Son, Tran Vu Thanh <tranvthanhson@gmail.com>
2017-10-20 12:03:37 +0700

    @ refactor code in shop view

--- app/views/admin/shop/index.view.php
+++ app/views/admin/shop/index.view.php
@@ -28,104 +28,99 @@
                                     <div class="clearfix"></div>
                                 </div>
                             </div>
-                           <?php if (isset($_SESSION['notice'])) {?>
-                            <div class="alert alert-success">
-                                <strong><?=$_SESSION['notice']?></strong>
-                            </div>
-                            <?php }?>
-                            <?php unset($_SESSION['notice']);?>
-                            <div class="content content-card table-responsive table-full-width">
-                                <table id="view-post" class="table table-hover table-striped">
-                                    <thead>
-                                        <th>#</th>
-                                        <th>Tên địa điểm</th>
-                                        <th>Ảnh</th>
-                                        <th>Danh mục</th>
-                                        <th>%</th>
-                                        <th>Khuyến mãi</th>
-                                        <th>Trạng thái</th>
-                                        <th>Bình luận</th>
-                                        <th>Chức năng</th>
-                                    </thead>
-                                    <tbody class="tableSearch">
-                                        <?php
-$index = 0;
-foreach ($shop['all'] as $value) {
-    $index++;
-    ?>
-                                        <tr>
-                                            <td><?=$index?></td>
-                                            <td class="name-place"><a href=""><?=$value->SHOP_NAME?></a></td>
-                                            <td class="img-post">
-                                                <a href=""><img  src="/public/admin/assets/img/img-shop/<?=$value->VIEW?>" /></a>
-                                            </td>
-                                            <td><?=$value->TYPE_NAME?></td>
-                                            <td class="change-discount<?=$value->SHOP_ID?>"><?=$value->DISCOUNT?></td>
-                                            <td class="percent-input">
-                                               <form action="javascript:void(0)">
-                                                    <div class="form-group">
-                                                       <div class="item-col">
-                                                           <input type="text" onkeypress="return isNumberKey(event)" maxlength="3" class="form-control" id="discount<?=$value->SHOP_ID?>" value="<?=$value->DISCOUNT?>" >
-                                                          <!--  <div class="help-block with-errors"></div> -->
-                                                       </div>
-                                                       <div class="item-col">
-                                                           <button type="submit" class="btn btn-success" onclick="changeDiscount(<?=$value->SHOP_ID?>)">Nhập</button>
-                                                       </div>
-                                                       <div class="clearfix"></div>
-                                                   </div>
-                                               </form>
-                                            </td>
-                                            <td>
-                                                <label class="checkbox">
-                                                    <?php if (1 == $value->STATUS) {?>
-                                                    <input type="checkbox" value="" data-toggle="checkbox" checked="" disabled="disabled">
-                                                    <?php } else {?>
-                                                    <input type="checkbox" value="" data-toggle="checkbox" disabled="disabled">
-                                                    <?php }?>
-                                                </label>
-                                            </td>
-                                            <td>
-                                                <div class="form-group">
-                                                    <div class="item-col">
-                                                        <a href="/admin/shop/comment?id=<?=$value->SHOP_ID?>" class="btn btn-warning" title="Xem">
-                                                            <i class="pe-7s-look"></i>
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td class="control">
-                                                <div class="form-group">
-                                                    <div class="item-col">
-                                                        <a href="/admin/shop/edit?id=<?=$value->SHOP_ID?>" class="btn btn-success" title="Sửa">
-                                                            <i class="pe-7s-note"></i>
-                                                        </a>
-                                                    </div>
-                                                    <div class="item-col">
-                                                        <a data-toggle="modal" data-target="#delpost<?=$value->SHOP_ID?>"  href="" class="btn btn-danger" title="Xoá">
-                                                            <i class="pe-7s-trash"></i>
-                                                        </a>
-                                                    </div>
-                                                    <div class="clearfix"></div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <?php view_include('admin.partials.modal', ['id_model' => 'delpost' . $value->SHOP_ID, 'title' => 'XÓA BÀI ĐĂNG ', 'content' => 'Bạn có chắc chắn muốn xóa không??', 'bt' => 'Xóa', 'link' => '/admin/shop/del?id=' . $value->SHOP_ID]);
-}?>
+                            <?php if (isset($_SESSION['notice'])) {?>
+                                <div class="alert alert-success">
+                                    <strong><?=$_SESSION['notice']?></strong>
+                                </div>
+                                <?php }?>
+                                <?php unset($_SESSION['notice']);?>
+                                <div class="content content-card table-responsive table-full-width">
+                                    <table id="view-post" class="table table-hover table-striped">
+                                        <thead>
+                                            <th>#</th>
+                                            <th>Tên địa điểm</th>
+                                            <th>Ảnh</th>
+                                            <th>Danh mục</th>
+                                            <th>%</th>
+                                            <th>Khuyến mãi</th>
+                                            <th>Trạng thái</th>
+                                            <th>Bình luận</th>
+                                            <th>Chức năng</th>
+                                        </thead>
+                                        <tbody class="tableSearch">
+                                            <?php $index = 0;?>
+                                            <?php foreach ($shop['all'] as $value): ?>
+                                                <tr>
+                                                    <td><?=++$index?></td>
+                                                    <td class="name-place"><a href=""><?=$value->SHOP_NAME?></a></td>
+                                                    <td class="img-post">
+                                                        <a href=""><img  src="/public/admin/assets/img/img-shop/<?=$value->VIEW?>" /></a>
+                                                    </td>
+                                                    <td><?=$value->TYPE_NAME?></td>
+                                                    <td class="change-discount<?=$value->SHOP_ID?>"><?=$value->DISCOUNT?></td>
+                                                    <td class="percent-input">
+                                                        <form action="javascript:void(0)">
+                                                            <div class="form-group">
+                                                                <div class="item-col">
+                                                                    <input type="text" onkeypress="return isNumberKey(event)" maxlength="3" class="form-control" id="discount<?=$value->SHOP_ID?>" value="<?=$value->DISCOUNT?>" >
+                                                                    <!--  <div class="help-block with-errors"></div> -->
+                                                                </div>
+                                                                <div class="item-col">
+                                                                    <button type="submit" class="btn btn-success" onclick="changeDiscount(<?=$value->SHOP_ID?>)">Nhập</button>
+                                                                </div>
+                                                                <div class="clearfix"></div>
+                                                            </div>
+                                                        </form>
+                                                    </td>
+                                                    <td>
+                                                        <?php $checked = '';?>
+                                                        <?php if (1 == $value->STATUS) {$checked = 'checked';}?>
+                                                        <div class="checked-box <?=$checked;?>">
+                                                            <i class="fa fa-check-square-o" aria-hidden="true"></i>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="form-group">
+                                                            <div class="item-col">
+                                                                <a href="/admin/shop/comment?id=<?=$value->SHOP_ID?>" class="btn btn-warning" title="Xem">
+                                                                    <i class="pe-7s-look"></i>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    <td class="control">
+                                                        <div class="form-group">
+                                                            <div class="item-col">
+                                                                <a href="/admin/shop/edit?id=<?=$value->SHOP_ID?>" class="btn btn-success" title="Sửa">
+                                                                    <i class="pe-7s-note"></i>
+                                                                </a>
+                                                            </div>
+                                                            <div class="item-col">
+                                                                <a data-toggle="modal" data-target="#delpost<?=$value->SHOP_ID?>"  href="" class="btn btn-danger" title="Xoá">
+                                                                    <i class="pe-7s-trash"></i>
+                                                                </a>
+                                                            </div>
+                                                            <div class="clearfix"></div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                <?php view_include('admin.partials.modal', ['id_model' => 'delpost' . $value->SHOP_ID, 'title' => 'XÓA BÀI ĐĂNG ', 'content' => 'Bạn có chắc chắn muốn xóa không??', 'bt' => 'Xóa', 'link' => '/admin/shop/del?id=' . $value->SHOP_ID]);?>
+                                            <?php endforeach;?>
 
-                                    </tbody>
-                                </table>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <?php $pagination = $shop?>
+                                <?php view_include('admin.partials.pagination', compact('pagination'));?>
                             </div>
-                             <?php $pagination = $shop?>
-                        <?php view_include('admin.partials.pagination', compact('pagination'));?>
                         </div>
                     </div>
                 </div>
             </div>
+            <?php view_include('admin.partials.footer');?>
         </div>
-        <?php view_include('admin.partials.footer');?>
     </div>
-</div>
-<!-- Modal -->
+    <!-- Modal -->
 
 
-<?php view_include('admin.layouts.foot-master');?>
+    <?php view_include('admin.layouts.foot-master');?>
