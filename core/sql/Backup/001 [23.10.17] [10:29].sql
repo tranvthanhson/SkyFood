9bf9a4ac4dd0662df7b94499e267f0cf61501446
Hoai Thuong <thuong.hth@neo-lab.vn>
2017-10-23 13:30:40 +0700

    @ change 001[23.10.17][10:29].sql

--- core/sql/Backup/001 [23.10.17] [10:29].sql
+++ core/sql/Backup/001 [23.10.17] [10:29].sql
@@ -1,3 +1,27 @@
+-- Drop account table
+DROP TABLE ACCOUNT;
+
+-- Drop save table
+DROP TABLE SAVE;
+
+-- Drop comment table
+DROP TABLE COMMENT;
+
+-- Drop rate table
+DROP TABLE RATE;
+
+-- Drop type table
+DROP TABLE TYPE;
+
+-- Drop shop table
+DROP TABLE SHOP;
+
+-- Drop type_shop table
+DROP TABLE TYPE_SHOP;
+
+-- Drop feedback table
+DROP TABLE FEEDBACK;
+
 -- phpMyAdmin SQL Dump
 -- version 4.7.1
 -- https://www.phpmyadmin.net/
