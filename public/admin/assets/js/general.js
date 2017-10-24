6686798c299505722ccf1a63e9bbb18f545672f6
quangong <ongvquang177@gmail.com>
2017-10-24 14:44:41 +0700

    + add function showloader() and hiddenloader() in search general.js

--- public/admin/assets/js/general.js
+++ public/admin/assets/js/general.js
@@ -1,5 +1,6 @@
  $('#search').click();
  function search(link){
+    showLoader();
     var key = $('#inputSearch').val();
    // alert(link);
    $.ajax({
@@ -11,6 +12,7 @@
             // aactive: active
         },
         success: function(data) {
+            hiddenLoader();
             $('.tableSearch').html(data);
             //alert($data);
         },
