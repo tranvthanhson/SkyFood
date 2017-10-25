9dd7a31267ebfbe536a20da4df199dda15f67768
quangong <ongvquang177@gmail.com>
2017-10-25 22:07:21 +0700

    @ edit ajaxcomment comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -42,9 +42,8 @@ class Comment extends Model
         $comment['SHOP_ID'] = $_POST['aid'];
         $comment['CONTENT'] = $_POST['avalue'];
         $comment['DATE_CREATED'] = $currentDate;
-        if ($_POST['avalue']) {
-            $this->insert($comment);
-        }
+
+        $this->insert($comment);
 
         $comments = $this->selectByShop($_POST['aid']);
         //die(var_dump($comments));
